36d7ad5b2a29c6dee127496303bdcf7a672962e9
Marius Tolzmann <marius@mineiros.io>
2021-09-29 18:33:54 +0200

    feat: Initial import from premium library

---
+++ output.tf
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------
+# OUTPUT CALCULATED VARIABLES (prefer full objects)
+# ------------------------------------------------------------------------------
+
+# remap iam to reduce one level of access (iam[]. instead of iam[].iam.)
+output "iam" {
+  description = "The iam resource objects that define the access to the secret"
+  value       = { for key, iam in module.iam : key => iam.iam }
+}
+
+# ------------------------------------------------------------------------------
+# OUTPUT ALL RESOURCES AS FULL OBJECTS
+# ------------------------------------------------------------------------------
+
+output "secret" {
+  description = "All `google_secret_manager_secret` resource attributes."
+  value       = try(google_secret_manager_secret.secret[0], null)
+}
+
+# ------------------------------------------------------------------------------
+# OUTPUT MODULE CONFIGURATION
+# ------------------------------------------------------------------------------
+
+output "module_enabled" {
+  description = "Whether the module is enabled."
+  value       = var.module_enabled
+}
---
+++ versions.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 0.14.7, < 2.0"
+
+  required_providers {
+    google = "~> 3.75"
+  }
+}
