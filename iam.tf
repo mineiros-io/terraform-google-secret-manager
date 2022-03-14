7d8224d8fd8a577f143410ca4d49239215bf36bd
Kassian Houben <kassian@mineiros.io>
2022-03-14 14:03:40 +1300

    fix: change tomap

--- iam.tf
+++ iam.tf
@@ -1,17 +1,21 @@
 locals {
-  iam_map = var.policy_bindings == null ? { for iam in var.iam : iam.role => iam } : tomap({})
+  iam_map = var.policy_bindings == null ? { for iam in var.iam : iam.role => iam } : {}
 
   policy_bindings = var.policy_bindings != null ? {
     iam_policy = {
       policy_bindings = var.policy_bindings
     }
-  } : tomap({})
+  } : {}
+
+  iam_output = [local.policy_bindings, local.iam_map]
+
+  iam_output_index = var.policy_bindings != null ? 0 : 1
 }
 
 module "iam" {
   source = "github.com/mineiros-io/terraform-google-secret-manager-iam?ref=v0.0.4"
 
-  for_each = var.policy_bindings != null ? local.policy_bindings : local.iam_map
+  local.iam_output[local.iam_output_index]
 
   module_enabled    = var.module_enabled
   module_depends_on = var.module_depends_on
