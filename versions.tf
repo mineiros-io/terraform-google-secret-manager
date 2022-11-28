5388fba7b179b5b454827b2b848abe21b3b510e0
Bence Wax <bence.wax@mineiros.io>
2022-11-28 15:35:34 +0100

    chore!: update from module template

--- versions.tf
+++ versions.tf
@@ -1,10 +1,11 @@
-terraform {
-  required_version = ">= 0.14.7, < 2.0"
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
 
+terraform {
+  required_version = "~> 1.0, != 1.1.0, != 1.1.1"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.75, < 5.0"
+      version = "~> 4.0"
     }
   }
 }
